6194ef18168356de6b090d4355cd65459e8a483f
Puni <Puni@DESKTOP-5V3KHU1>
2018-07-02 19:16:27 +0530

    Done

--- src/divA/OathOfNightsWatch.java
+++ src/divA/OathOfNightsWatch.java
@@ -1,9 +1,32 @@
 package divA;
 
+import java.util.Scanner;
+
 public class OathOfNightsWatch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for(int i = 0;i < n ;i++){
+			arr[i] = scn.nextInt();
+			if(arr[i] < min){
+				min = arr[i];
+			}
+			if(arr[i] > max){
+				max = arr[i];
+			}
+	
+		}
+		int count = 0;
+		for(int i = 0;i < arr.length;i++){
+			if(arr[i] > min && arr[i] < max){
+				count++;
+			}
+		}
+		System.out.println(count);
 
 	}
 
